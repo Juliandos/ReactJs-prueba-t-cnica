9c2ba8c71a0de8e72c5c9a6e948abc808cbbe757
Juliandos <95juliandos@gmail.com>
2025-03-03 13:32:55 -0600

    estilos modificados exitosamente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginForm from "./components/LoginForm";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      document.body.style.background = "linear-gradient(135deg, rgb(124, 208, 238), rgb(5, 105, 196))";
+    } else {
+      document.body.style.background = "linear-gradient(135deg, black, purple, gray)";
+    }
+  }, [isAuthenticated]);
 
   return isAuthenticated ? (
     <Dashboard setIsAuthenticated={setIsAuthenticated} />
